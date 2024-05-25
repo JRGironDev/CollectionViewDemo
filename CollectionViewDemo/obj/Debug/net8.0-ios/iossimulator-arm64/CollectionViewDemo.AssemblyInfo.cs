e91f5918decc339c1c5316f4354ba36fa10ced83
José Quiñónez <joserobertoq@outlook.com>
2024-05-25 08:13:22 -0600

    Add a Costum Empty View

--- CollectionViewDemo/obj/Debug/net8.0-ios/iossimulator-arm64/CollectionViewDemo.AssemblyInfo.cs
+++ CollectionViewDemo/obj/Debug/net8.0-ios/iossimulator-arm64/CollectionViewDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollectionViewDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+46715d50c151a6ba1bded487c892ce41ceb2c46a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b6ce94dbcdb27cf9efca3987092797ebc69a9cca")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollectionViewDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollectionViewDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
