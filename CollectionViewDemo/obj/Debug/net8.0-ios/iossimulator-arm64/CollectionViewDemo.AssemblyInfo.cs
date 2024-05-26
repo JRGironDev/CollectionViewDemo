54b72392ba641dfa50ed5cb4c3a731d76df58d7f
José Quiñónez <joserobertoq@outlook.com>
2024-05-26 06:40:56 -0600

    Add GroupHeaderTemplate

--- CollectionViewDemo/obj/Debug/net8.0-ios/iossimulator-arm64/CollectionViewDemo.AssemblyInfo.cs
+++ CollectionViewDemo/obj/Debug/net8.0-ios/iossimulator-arm64/CollectionViewDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollectionViewDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3bda8c57fef671437c6a356275771b0161b47217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b0476ff038a9624b4e48499a29e8382a3a5914d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollectionViewDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollectionViewDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
